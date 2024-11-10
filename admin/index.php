c40ed4f58e7ec5195b022824f7150b230c266fd7
Youssef-Hammi <youssef.hammi.pm@gmail.com>
2024-11-10 17:37:30 +0100

    V1: Background du project Ecommerce

---
+++ admin/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include("nav.php");
+    ?>
+    <div class="container mt-3">
+        <h1>Listes Des Produits</h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, eos, officia asperiores sunt corrupti voluptate nihil vitae, laudantium unde dolor nam. Officiis quis cupiditate minus corrupti reprehenderit delectus veritatis alias!
+        Veniam modi laudantium officia odit temporibus repellendus architecto culpa, tenetur cum, repellat dicta iure. Facere fugit blanditiis dolore sint, similique, sit minus voluptatum delectus debitis asperiores doloribus, ratione possimus repudiandae.
+        Maxime temporibus aperiam possimus recusandae officia totam voluptates laborum tenetur at? Facilis voluptas voluptate quos odio, praesentium nostrum assumenda. Voluptatem eos numquam ducimus sapiente omnis, labore quo consequatur ratione vel?
+        Ullam fugit accusamus tempore sunt molestiae consectetur inventore, eveniet voluptatibus, quas dolore suscipit amet eos ad impedit similique iure, sapiente natus beatae at corporis sequi itaque soluta! Sint, pariatur saepe.
+        Voluptates beatae quo dignissimos officia velit molestiae obcaecati tempora inventore voluptatum facere, accusantium voluptatem at cupiditate perferendis minus quos saepe adipisci enim, debitis suscipit voluptate ipsa! Dolorum ad nam ea?
+        Veniam perspiciatis facilis doloremque expedita eveniet, placeat nisi autem iste maxime dolore earum culpa nulla laboriosam saepe labore facere explicabo aperiam quae optio dicta. Omnis corporis commodi perspiciatis iste mollitia.
+        Magni qui incidunt cum quas laudantium libero odit? Illo nobis nihil voluptate, id quo atque reprehenderit facere temporibus fuga tenetur ratione sequi repellendus reiciendis vero veniam nostrum amet nesciunt provident!
+        Commodi perferendis nobis incidunt unde. Voluptate tempora eaque veniam quae nam, eum dolorum tempore! Atque ipsam velit ad earum aliquam debitis fugiat soluta ex laudantium quam? Facilis pariatur voluptas suscipit.
+        Perspiciatis tenetur eaque ea sunt mollitia commodi, at esse ut dignissimos velit deleniti nihil enim, facere corporis! Expedita autem ut illum ducimus repellat porro harum sapiente beatae voluptatem, unde nam?
+        Cumque aperiam dolorum magni incidunt ea quidem nam debitis ipsa rerum et laudantium, illo numquam asperiores est repellendus sapiente quod delectus dolor aliquid. Laudantium distinctio at, repellat ad totam sunt.
+        Exercitationem animi doloremque voluptates necessitatibus inventore, recusandae, nisi perspiciatis numquam, dicta tenetur reiciendis alias repellat modi magni illo! Laboriosam obcaecati est odit eum culpa esse ipsam nam cumque dolorem dicta!
+        Possimus, officiis nihil ab sequi repudiandae praesentium amet, at illum officia tempore consequuntur enim sint quia. Eligendi magnam illo autem. Architecto, harum. Expedita cupiditate provident ex dolor soluta voluptatibus nulla.
+        Aperiam, quasi? Officia error saepe vitae, architecto ad laudantium? Rerum accusamus quaerat hic obcaecati omnis similique odio labore. Autem aperiam, sapiente debitis cumque labore provident officia quas iusto error at.
+        Dolorum quas, corrupti sunt architecto soluta nam. Dignissimos eius nobis nostrum a praesentium, quae ipsa soluta vel, blanditiis itaque hic maxime fugiat libero laboriosam doloribus! Minus ullam repudiandae officiis quae!
+        Nihil, molestiae saepe repellendus odio animi laboriosam vitae laborum eum eveniet! Vero dolore quo, pariatur fugiat quas molestias quidem eius? Tenetur vero quae excepturi corporis earum labore nisi asperiores quia.
+    </div>
+</body>
+
+</html>
---
+++ footer.php
@@ -0,0 +1,28 @@
+<hr>
+<h2>Service Irreprochable</h2>
+<div class="row">
+    <div class="col-4">
+        <div class="card bg-primary text-white text-center">
+            <div class="card-body">
+                <i class="fas fa-truck fa-3x"></i>
+                <p class="card-test">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam magni in qui enim esse!</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card bg-success text-white text-center">
+            <div class="card-body">
+                <i class="fas fa-lock fa-3x"></i>
+                <p class="card-test">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam magni in qui enim esse!</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card bg-warning text-white text-center">
+            <div class="card-body">
+                <i class="fas fa-check fa-3x"></i>
+                <p class="card-test">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam magni in qui enim esse!</p>
+            </div>
+        </div>
+    </div>
+</div>
